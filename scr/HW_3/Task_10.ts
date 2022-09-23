c47ebfe17e095bde1e707e505ba9ec8993eabc64
Aleksander Avtushko <aleksandravtusko_gmail_com>
2022-09-23 22:48:33 +0300

    feat: add HW3 tasks 9 - 13

---
+++ scr/HW_3/Task_10.ts
@@ -0,0 +1,18 @@
+//#### Task 10 💻
+
+let key = true;
+let documents = true;
+let pen = true;
+let apple = true;
+let orange = false;
+
+if (key && documents && pen && (apple || orange)) {
+  const shouldGoToWork = "you can go to work";
+  console.log(shouldGoToWork);
+}   else {
+     const shouldGoToWork = "you can't go to work";
+     console.log(shouldGoToWork);
+}
+
+
+
---
+++ scr/HW_3/Task_11.ts
@@ -0,0 +1,11 @@
+// #### Task 11 💻
+
+let num = 5;
+
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log("FizBuz");
+} else if (num % 5 === 0 ) {
+  console.log("Fiz");
+} else if (num % 3 === 0) {
+  console.log("Buz");
+}
