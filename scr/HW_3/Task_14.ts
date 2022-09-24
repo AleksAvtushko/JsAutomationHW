5d758e141c0da80d4a51ec1441f2c1a3bc44e065
Aleksander Avtushko <aleksandravtusko_gmail_com>
2022-09-24 12:21:23 +0300

    feat: add HW3 tasks 14 - 15

---
+++ scr/HW_3/Task_14.ts
@@ -0,0 +1,15 @@
+/*### ADVANCED level
+
+#### Task 1 👨‍🏫 */
+
+    let a = 4;
+    let b = 3;
+
+    if (a === 4 ) {
+      console.log(3);
+    }
+
+    if (b === 3) {
+      console.log(4);
+    }
+
