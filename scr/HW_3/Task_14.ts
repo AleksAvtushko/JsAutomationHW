db8b2e7ca4631f29ab80481dec2e6f99327ba631
Aleksander Avtushko <aleksandravtusko_gmail_com>
2022-09-28 22:14:23 +0300

    fix: added new calculation

--- scr/HW_3/Task_14.ts
+++ scr/HW_3/Task_14.ts
@@ -2,14 +2,11 @@
 
 #### Task 1 👨‍🏫 */
 
-    let a = 4;
-    let b = 3;
+let a = 4;
+let b = 3;
 
-    if (a === 4 ) {
-      console.log(3);
-    }
+a = a + b;
+b = a - b;
+a = a - b;
 
-    if (b === 3) {
-      console.log(4);
-    }
-
+console.log(a, b);
