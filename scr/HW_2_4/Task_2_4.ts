86d764ad1960efee3347200c856c1ba67ef57be2
Aleksander Avtushko <aleksandravtusko_gmail_com>
2022-11-15 19:28:11 +0300

    fix: HW 2_4 Task 4: added camel case and changed security on private

--- scr/HW_2_4/Task_2_4.ts
+++ scr/HW_2_4/Task_2_4.ts
@@ -4,36 +4,36 @@
 которые в зависимости от переданных аргументов и ожидаемого результата выводит либо: что "тест" прошел, либо что "тест" свалился */
 
 class Calculator {
-    constructor(protected value1: number, protected value2: number) {}
+    constructor(private value1: number, private value2: number) {}
 
-    getaddition() {
+    getAddition() {
         return this.value1 + this.value2;
     }
-    getsubtraction() {
+    getSubtraction() {
         return this.value1 - this.value2;
     }
 
-    getmultiply() {
+    getMultiply() {
         return this.value1 * this.value2;
     }
 
-    getdivide() {
+    getDivide() {
         return this.value1 / this.value2;
     }
 
-    getdegree() {
+    getDegree() {
         return this.value1 ** this.value2;
     }
 }
 const sumCalculator = new Calculator(4, 4);
 
-console.log(sumCalculator.getaddition() === 8 ? "Test 1 passed" : "Test 1 failed");
-console.log(sumCalculator.getsubtraction() === 0 ? "Test 2 passed" : "Test 2 failed");
-console.log(sumCalculator.getmultiply() === 16 ? "Test 3 passed" : "Test 3 failed");
-console.log(sumCalculator.getdivide() === 1 ? "Test 4 passed" : "Test 4 failed");
-console.log(sumCalculator.getdegree() === 256 ? "Test 5 passed" : "Test 5 failed");
-console.log(sumCalculator.getaddition() === 18 ? "Test 6 passed" : "Test 6 failed");
-console.log(sumCalculator.getsubtraction() === 8 ? "Test 7 passed" : "Test 7 failed");
-console.log(sumCalculator.getmultiply() === 8 ? "Test 8 passed" : "Test 8 failed");
-console.log(sumCalculator.getdivide() === 8 ? "Test 9 passed" : "Test 9 failed");
-console.log(sumCalculator.getaddition() === 7 ? "Test 10 passed" : "Test 10 failed");
+console.log(sumCalculator.getAddition() === 8 ? "Test 1 passed" : "Test 1 failed");
+console.log(sumCalculator.getSubtraction() === 0 ? "Test 2 passed" : "Test 2 failed");
+console.log(sumCalculator.getMultiply() === 16 ? "Test 3 passed" : "Test 3 failed");
+console.log(sumCalculator.getDivide() === 1 ? "Test 4 passed" : "Test 4 failed");
+console.log(sumCalculator.getDegree() === 256 ? "Test 5 passed" : "Test 5 failed");
+console.log(sumCalculator.getAddition() === 18 ? "Test 6 passed" : "Test 6 failed");
+console.log(sumCalculator.getSubtraction() === 8 ? "Test 7 passed" : "Test 7 failed");
+console.log(sumCalculator.getMultiply() === 8 ? "Test 8 passed" : "Test 8 failed");
+console.log(sumCalculator.getDivide() === 8 ? "Test 9 passed" : "Test 9 failed");
+console.log(sumCalculator.getAddition() === 7 ? "Test 10 passed" : "Test 10 failed");
